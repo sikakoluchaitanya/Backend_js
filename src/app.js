7930005a08f6ea244ebd3392dab598a4c9af724f
sikakoluchaitanya <sikakoluchaitaya@gmail.com>
2024-09-15 13:20:03 +0530

    defined routes,multer

--- src/app.js
+++ src/app.js
@@ -14,5 +14,15 @@ app.use(express.urlencoded({extended: true, limit: "20kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// routes import 
+
+import userRouter from './routes/user.routes.js';
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter); // this will have the url http://localhost:8000/api/v1/users
+
+
 export { app };
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        //upload the file to cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("file uploaded successfully: ", response.url);
+        return response.url;
+    } catch (error) {
+        fs.unlink (localFilePath) // remove the locally stored file cause the upload failed
+    }
+}
