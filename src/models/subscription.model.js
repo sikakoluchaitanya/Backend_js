9f365881e9838839e5e69abafee741b0644ae711
sikakoluchaitanya <sikakoluchaitanya@gmail.com>
2024-09-17 06:43:16 +0530

    fixed typo and added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, //the one who is Subscribing
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, //the one who is subscribed
+        ref: "User"
+    }
+},{ timestamps: true })
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
